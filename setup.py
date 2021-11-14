c0c6282472cf5136c808edf669bdef4be6f286ec
shreyasingh1 <40285923+shreyasingh1@users.noreply.github.com>
2021-11-14 17:14:19 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='epilepsypcm',
     version='0.0.0',
-    packages=['pcm', 'pcm.loading'],
+    packages=['epilepsypcm', 'epilepsypcm.loading'],
     url='',
     license='',
     author='shrey2',
