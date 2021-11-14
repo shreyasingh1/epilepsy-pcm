5e06215bb562c3d4727c87183ec26099dd06d075
Shreya Singh <shrey2@macbook-pro-3.lan>
2021-11-14 17:02:12 -0500

    created imports folder for mark's functions finished dataloading_examples notebook created setup.py file, set epilepsypcm up like python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='epilepsypcm',
+    version='',
+    packages=['pcm', 'pcm.loading'],
+    url='',
+    license='',
+    author='shrey2',
+    author_email='ssingh93@jhu.edu',
+    description=''
+)
